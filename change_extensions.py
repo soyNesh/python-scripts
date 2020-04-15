f803893e937401b24232a55c0524d089f0c38d18
NeshMx <alex.huerta0424@hotmail.com>
2020-04-15 00:18:25 -0500

    Adds null replacement handling

--- change_extensions.py
+++ change_extensions.py
@@ -1,10 +1,14 @@
 import os
 import sys
+
 folder = sys.argv[1]
+original_extension = sys.argv[2]
+replacement = sys.argv[3] if sys.argv[3] != "" else ""
+
 for filename in os.listdir(folder):
     infilename = os.path.join(folder, filename)
     if not os.path.isfile(infilename):
         continue
     oldbase = os.path.splitext(filename)
-    newname = infilename.replace('.webp', '')
+    newname = infilename.replace(original_extension, replacement)
     output = os.rename(infilename, newname)
